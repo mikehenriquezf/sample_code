c5533a11b4be52b75a67bdc86d54a4bc7461da22
Mike Henriquez <mike.henriquezf@gmail.com>
2023-12-04 15:36:15 -0400

    version php y apache

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'PHP version: ' . phpversion();
+// Obtén la versión de Apache
+$apacheVersion = apache_get_version();
+
+// Obtén la versión de PHP
+$phpVersion = phpversion();
+
+// Imprime la información
+echo "Versión de Apache: $apacheVersion<br>";
+echo "Versión de PHP: $phpVersion";
+?>
